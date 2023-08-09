6c27bcae788773a778c8482f8c3f3012f6adf98c
Vinícius de Castilhos <141281219+castilhoss@users.noreply.github.com>
2023-08-09 18:58:03 -0300

    Add files via upload

---
+++ exercicio 031.py
@@ -0,0 +1,9 @@
+distancia = float(input("Qual é a distância da sua viagem? "))
+
+if distancia <= 200:
+    preco = distancia * 0.50
+    print("O preço da sua passagem será de R${:.2f}".format(preco))
+else:
+    preco = distancia * 0.45
+    print("O preço da sua passagem será de R${:.2f}".format(preco))     
+
