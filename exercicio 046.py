8ee6b88ce64bc1c6913775bc011b6136ff22cf1b
Vinícius de Castilhos <141281219+castilhoss@users.noreply.github.com>
2023-08-13 23:18:58 -0300

    Add files via upload

---
+++ exercicio 046.py
@@ -0,0 +1,6 @@
+from time import sleep
+for count in range(10, 0, -1):
+    sleep(1)
+    print(count)
+sleep(1)
+print("KABUMMMMM!")
---
+++ exercicio 047.py
@@ -0,0 +1,6 @@
+from time import sleep
+for count in range(0, 51, 2):
+    print(count)
+    sleep(1)
+
+print("Uhullll números pares")
